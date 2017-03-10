b757aede8ac126d8cbd30bf55181124aa793c57d
Scott Ouellette <scottx611x@gmail.com>
2017-03-10 15:13:58 -0500

    Allow blank fields and add slugfield

--- core/models.py
+++ core/models.py
@@ -2,9 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from markdownx.models import MarkdownxField, MarkdownxFormField
+from markdownx.models import MarkdownxField
 
-class MyModel(models.Model):
+class MarkdownPage(models.Model):
+    slug = models.SlugField(unique=True)
     markdownx_field1 = MarkdownxField()
-    markdownx_field2 = MarkdownxField()
-    markdownx_field3 = MarkdownxField()
+    markdownx_field2 = MarkdownxField(blank=True)
+    markdownx_field3 = MarkdownxField(blank=True)
+
+    def __str__(self):
+        return self.slug
